7fbc0a082777c67daa2b2baca54b0ffbcabc26e0
Yann N'DA <yannnda97@gmail.com>
2026-03-26 15:07:07 +0100

    Fix home.php route + Initialize HtmlResponse.php

--- frontend/views/pages/home.php
+++ frontend/views/pages/home.php
@@ -3,4 +3,4 @@
 declare(strict_types=1);
 ?>
 <h1 class="title">Bienvenue</h1>
-<p class="lead">Structure minimale : PHP vanilla, vues dans <code>views/</code>, assets dans <code>public/assets/</code>.</p>
+<p class="lead">Structure minimale : PHP vanilla, vues dans <code>frontend/views/</code>, assets dans <code>public/assets/</code>.</p>
