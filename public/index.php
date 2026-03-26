a38ae9a05781a8fec810ab872053215274d28ed0
KelianHalleray <kelianhalleray@gmail.com>
2026-03-26 15:03:00 +0100

    feat: add login , signin and register pages

--- public/index.php
+++ public/index.php
@@ -5,14 +5,21 @@ declare(strict_types=1);
 require_once __DIR__ . '/../src/Http/HtmlResponse.php';
 require_once __DIR__ . '/../src/Http/JsonResponse.php';
 require_once __DIR__ . '/../src/Http/Router.php';
+require_once __DIR__ . '/../src/Http/Cors.php';
+require_once __DIR__ . '/../src/Http/Session.php';
 require_once __DIR__ . '/../src/Infrastructure/Database.php';
 require_once __DIR__ . '/../src/Controllers/HealthController.php';
+require_once __DIR__ . '/../src/Controllers/LoginController.php';
 
 use App\Controllers\HealthController;
 use App\Http\HtmlResponse;
+use App\Controllers\LoginController;
+use App\Http\Cors;
 use App\Http\JsonResponse;
 use App\Http\Router;
 use App\Infrastructure\Database;
+
+Cors::handle();
 
 try {
     $router = new Router();
@@ -28,6 +35,19 @@ try {
     $healthController = new HealthController();
     $router->get('/health', static function () use ($healthController): void {
         $healthController->get();
+    });
+
+    $loginController = new LoginController();
+    $router->post('/login', static function () use ($loginController): void {
+        $loginController->login();
+    });
+
+    $router->post('/register', static function () use ($loginController): void {
+        $loginController->register();
+    });
+
+    $router->post('/logout', static function () use ($loginController): void {
+        $loginController->logout();
     });
 
     $router->get('/db/ping', static function (): void {
